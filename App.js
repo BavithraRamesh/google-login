52024dc87d747d7054e36ad05e949ebf01931df4
BavithraRamesh <73887529+BavithraRamesh@users.noreply.github.com>
2020-11-03 17:42:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import { enableScreens } from 'react-native-screens';
+import Providers from './navigation';
+
+const App = () => {
+  return <Providers />;
+}
+
+export default App;
